1d3fe71b3c8e0c177b3b6a401f87974311a597ed
Arthur Henrique Franco da Silva <1051698@senacminas.edu.br>
2026-05-14 16:40:22 -0300

    Impletar funcinalidade lightbox

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ function mudarTema() {
         body.classList.add("claro");
     }
 }
+
+function abrirlightbox(element){
+    var lightbox = document.getElementById("lightbox");
+    var lightbox_img = document.getElementById("lightbox-img");
+
+    lightbox_img.src = element.src;
+    lightbox.style.display = "flex";
+} 
+
+
+function fecharlightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
